8d7646a295bcc9e78aac646c4a8d8f0abb2f4d08
Rishabh Bali <rishabhsbali@gmail.com>
2022-06-29 16:35:28 +0530

    Initial template

---
+++ include/Interface/Container.h
@@ -0,0 +1,54 @@
+//===- Container.h --------------------------------------------------------===//
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//===----------------------------------------------------------------------===//
+//
+// Container descriptor.
+//
+//===----------------------------------------------------------------------===//
+
+
+#ifndef INTERFACE_GRAPH_CORE_CONTAINER_H
+#define INTERFACE_GRAPH_CORE_CONTAINER_H
+
+#include <memory>
+#include <stdint.h>
+#include <vector>
+
+// MemRef descriptor.
+// - T represents the type of the elements.
+// - N represents the number of dimensions.
+template <typename T, size_t N> class MemRef {
+
+protected:
+  // Default constructor.
+  MemRef() {};
+
+  // Data.
+  // The `aligned` and `allocated` members point to the same address, `aligned`
+  // member is responsible for handling data, and `allocated` member is
+  // resposible for handling the memory space.
+  T *allocated;
+  T *aligned;
+  // Offset.
+  intptr_t offset = 0;
+  // Shape.
+  intptr_t sizes[N];
+  // Strides.
+  intptr_t strides[N];
+  // Number of elements.
+  size_t size;
+};
+
+#endif // INTERFACE_GRAPH_CORE_CONTAINER. 
---
+++ lib/Interface/GraphContainer.cpp
@@ -0,0 +1,42 @@
+//===- GraphContainer.cpp -------------------------------------------------===//
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the Graph container descriptor.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef GRAPH_CONTAINER_DEF
+#define GRAPH_CONTAINER_DEF
+
+#include "Interface/Container.h"
+#include "include/Interface/GraphContainer.h"
+
+// Adjaceny Matrix constructor.
+template <typename T, size_t N> Graph<T, N>::Graph(T V, T **adjMatrix) : MemRef<T, N>() {
+    this->size[0] = V;
+    this->size[1] = V;
+    this->strides[0] = V;
+    this->strides[1] = V;
+    this->allocated = new T[this->size];
+    //TODO
+    // Add the implementation for passing data to alignned.
+};
+
+
+// TODO
+// Implement the contructor for different implementation.
+
+#endif // GRAPH_CONTAINER_DEF 
