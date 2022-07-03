6498d2de8999f1363300eda419d811c48c7cde04
sarrah-basta <sarrah.basta@gmail.com>
2022-07-03 20:15:31 +0530

    basic container template for adjacency list

--- examples/bfsExample.cpp
+++ examples/bfsExample.cpp
@@ -10,20 +10,28 @@
 #include <Interface/graph.h>
 #include <Interface/memref.h>
 #include <iostream>
+#include "../lib/Interface/GraphContainer.cpp"
 
 int main() {
   std::cout << "Reached here !!!\n";
 
-  float sample_graph1_array[9] = {1, 1, 1, 1, -8, 1, 1, 1, 1};
-  intptr_t sample_graph_length = 3;
-  intptr_t sample_graph_width = 3;
-  float *allocation_pointer = (float *)malloc(sizeof(float));
-  intptr_t sample_graph_sizes[2] = {sample_graph_width, sample_graph_length};
-  intptr_t sample_graph_strides[2] = {sample_graph_width, sample_graph_length};
+  // float sample_graph1_array[9] = {1, 1, 1, 1, -8, 1, 1, 1, 1};
+  // intptr_t sample_graph_length = 3;
+  // intptr_t sample_graph_width = 3;
+  // float *allocation_pointer = (float *)malloc(sizeof(float));
+  // intptr_t sample_graph_sizes[2] = {sample_graph_width, sample_graph_length};
+  // intptr_t sample_graph_strides[2] = {sample_graph_width, sample_graph_length};
+  intptr_t no_of_elements;
+  std::cout<<"enter the number of elements in graph"<<std::endl;
+  std::cin>>no_of_elements;
 
-  MemRef_descriptor sample_graph =
-      MemRef_Descriptor(allocation_pointer, sample_graph1_array, 0,
-                        sample_graph_sizes, sample_graph_strides);
+  AdjGraph<float> my_graph(no_of_elements);
+  //TODO for Adjacency List Directed 
+  // Call functions to create graph
+  // Convert data to Memref for passing in mlir
+  // MemRef_descriptor sample_graph =
+  //     MemRef_Descriptor(allocation_pointer, sample_graph1_array, 0,
+  //                       sample_graph_sizes, sample_graph_strides);
 
-  graph::graph_bfs(sample_graph, sample_graph, sample_graph);
+  // graph::graph_bfs(sample_graph, sample_graph, sample_graph);
 }
