ef558d9670cccb524900e569c596b9631df2f23c
Rishikesh Donadkar <rishikeshdonadkar@gmail.com>
2022-07-19 18:50:33 +0530

    Frontend: Graph accepting adjacency list.
    Added the forntend where edges are added in the adjacency list type
    of graph implementation.
    
    Signed-off-by: Rishikesh Donadkar <rishikeshdonadkar@gmail.com>

--- examples/bfsExample.cpp
+++ examples/bfsExample.cpp
@@ -9,21 +9,22 @@
 
 #include <Interface/graph.h>
 #include <Interface/memref.h>
+#include <Interface/Container.h>
+#include <Interface/GraphContainer.h>
+#include <vector>
 #include <iostream>
 
 int main() {
-  std::cout << "Reached here !!!\n";
-
-  float sample_graph1_array[9] = {1, 1, 1, 1, -8, 1, 1, 1, 1};
-  intptr_t sample_graph_length = 3;
-  intptr_t sample_graph_width = 3;
-  float *allocation_pointer = (float *)malloc(sizeof(float));
-  intptr_t sample_graph_sizes[2] = {sample_graph_width, sample_graph_length};
-  intptr_t sample_graph_strides[2] = {sample_graph_width, sample_graph_length};
-
-  MemRef_descriptor sample_graph =
-      MemRef_Descriptor(allocation_pointer, sample_graph1_array, 0,
-                        sample_graph_sizes, sample_graph_strides);
-
-  graph::graph_bfs(sample_graph, sample_graph, sample_graph);
+  
+  	Graph<float, 2> sample_graph(graph::detail::GRAPH_ADJ_LIST_UNDIRECTED_WEIGHTED, 4);	
+	 
+	sample_graph.addEdge(0,2,3);
+	sample_graph.addEdge(1,2,3);
+	sample_graph.addEdge(2,3,3);
+	sample_graph.addEdge(3,2,4);
+	
+	auto memref = sample_graph.graph_to_MemRef_descriptor();
+	
+	//this will print the linear 2d matrix in 2d form.
+	sample_graph.printGraph();
 }
---
+++ include/Interface/GraphContainer.h
@@ -0,0 +1,58 @@
+//===- GraphContainer.h ---------------------------------------------------===//
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//===----------------------------------------------------------------------===//
+//
+// Graph container descriptor.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef INTERFACE_GRAPHCONTAINER_H
+#define INTERFACE_GRAPHCONTAINER_H
+
+#include "Interface/Container.h"
+#include "Interface/memref.h"
+#include <cstddef>
+#include <iostream>
+
+template <typename T, size_t N> class Graph : public MemRef<T, N> {
+	protected:
+		//the graph type and representation
+		uint16_t graph_type;
+
+		//the count of number of edges added in the graph.
+		int edgeCount = 0;
+	
+		//adjacency list representation of graph.
+		std::vector<std::vector<T>> adjList;
+		
+		//weighted adjacency list representation of graph.
+		std::vector<std::vector<std::pair<T,T>>> adjList_weighted;
+	public:
+		//Constructor
+		Graph(uint16_t graph_type, size_t size);
+
+		//Function to add edges in graph.
+		void addEdge(T a, T b);
+		void addEdge(T Node1,T Node2, T EdgeWeight); 
+		
+		//converter from graph to MemRef_descriptor
+		MemRef_descriptor graph_to_MemRef_descriptor();
+
+		//Function to print the linear 2d graph.
+		void printGraph();
+};
+
+#include "Interface/GraphContainer.cpp"
+#endif // INTERFACE_GRAPHCONTAINER_H 
